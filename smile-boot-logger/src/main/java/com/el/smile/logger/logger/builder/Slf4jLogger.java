c0207916d711c3d75c7de3ba6584cc73b6efa5d9
eddie.lys <liyingshi@liuheqinqiang.com>
2021-03-05 16:43:36 +0800

    update: 获取上级路径

--- smile-boot-logger/src/main/java/com/el/smile/logger/logger/builder/Slf4jLogger.java
+++ smile-boot-logger/src/main/java/com/el/smile/logger/logger/builder/Slf4jLogger.java
@@ -100,10 +100,12 @@ public class Slf4jLogger extends BaseLoggerBuilder {
         String logPath = StringUtils.trim(StringUtils.substring(logBasePath, index));
         if (StringUtils.startsWith(logBasePath, SmileLoggerConstants.RELATIVE)) {
             String property = System.getProperty("user.dir");
+            File targetPath = new File(property);
+
             if (StringUtils.startsWith(logPath, File.separator)) {
-                return property.concat(logPath);
+                return targetPath.getParent().concat(logPath);
             }
-            return property.concat(File.separator).concat(logPath);
+            return targetPath.getParent().concat(File.separator).concat(logPath);
         }else if (StringUtils.startsWith(logBasePath, SmileLoggerConstants.ABSOLUTE)) {
             if (StringUtils.startsWith(logPath, File.separator)) {
                 return logPath;
